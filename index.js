7e42884e6303a13dc749a77fb5db20e24f810031
Wasit Faraizi <wasit.allin1@gmail.com>
2022-07-23 01:35:47 +0600

    added functionality to buttons

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // UI variables
 const navLogo = document.querySelector('#nav-logo')
+const homeBtn = document.getElementById('home-btn')
+const aboutBtn = document.getElementById('about-btn')
+const menuBtn = document.getElementById('menu-btn')
+const contactBtn = document.getElementById('contact-btn')
+const navBtns = document.querySelectorAll('.nav-btn')
 
 window.onscroll = function () {
   hideLogo()
