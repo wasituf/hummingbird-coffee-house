4b10042206aa4408ae937d594956149e9e1f402a
Wasit Faraizi <wasit.allin1@gmail.com>
2022-07-23 23:02:48 +0600

    added highlighting for nav buttons when clicked

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // UI variables
 const navLogo = document.querySelector('#nav-logo')
-const homeBtn = document.getElementById('home-btn')
-const aboutBtn = document.getElementById('about-btn')
-const menuBtn = document.getElementById('menu-btn')
-const contactBtn = document.getElementById('contact-btn')
 const navBtns = document.querySelectorAll('.nav-btn')
 
 window.onscroll = function () {
   hideLogo()
 }
+
+Array.from(navBtns).forEach(btn =>
+  btn.addEventListener('click', () => {
+    Array.from(navBtns).forEach(btn => btn.classList.remove('current'))
+    btn.classList.add('current')
+  }),
+)
 
 function hideLogo() {
   if (document.documentElement.scrollTop > 100) {
